9c852e3e8950624618815dc692079398b72943e5
Rough-way <sheikh28a@gmail.com>
2024-01-24 17:37:11 +0300

    added health bar

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    
+    public static PlayerState Instance { get; set; }
+    // ---- Player Health ---- //
+    public float currentHealth;
+    public float maxHealth;
+
+
+    
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+
+    }
+
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            currentHealth -= 10;
+        }
+
+    }
+}
+
+
+
+
+
+
+
